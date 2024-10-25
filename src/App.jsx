3c0d7ba2014a003d535f4a1ac82268ec607a079b
Danil <dabcom2013@gmail.com>
2024-10-25 22:19:16 +0300

    add log in form

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import cl from './components/login/Login.module.css'
+import companyLogo from './imgs/oggetto-logo_tonal-hor-eng_tonal-hor-eng.png'
 
 export default function App() {
   return (
     <div>
       <div className={cl.login__container}>
-        <h1>Title</h1>
-          <form className={cl.my_form + ' ' + cl.login__content}>
-        <div className={cl.form_component}>
-          <input className={cl.form_input} type="login" name="login" id="loginName" />
-          <label  htmlFor="loginName">Username</label>
-        </div>
+        <img className={cl.form__image} src={companyLogo} alt='logo'/>
+        <form className={cl.my_form + ' ' + cl.login__content}>
+          <div className={cl.form_component}>
+            <input className={cl.form_input} type="login" name="login" id="loginName" />
+            <label htmlFor="loginName">Username</label>
+          </div>
 
-        <div className={cl.form_component}>
-          <input className={cl.form_input} type="password" name="password" id="passwordName"/>
-          <label htmlFor="passwordName">Password</label>
-        </div>
+          <div className={cl.form_component}>
+            <input className={cl.form_input} type="password" name="password" id="passwordName" />
+            <label htmlFor="passwordName">Password</label>
+          </div>
 
-        <button className={cl.form_btn} type='submit'>Log in</button>
-      </form>
+          <button className={cl.form_btn} type='submit'>Log in</button>
+        </form>
       </div>
     </div>
   )
